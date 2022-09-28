b9efc5f24bf82960d806ebbe208a4c0b84047b50
zhongpei <1000@intry.cn>
2022-09-28 11:48:53 +0800

    0

--- lib/src/entities/rooms/room_detail_dto.dart
+++ lib/src/entities/rooms/room_detail_dto.dart
@@ -1,7 +1,7 @@
-import 'package:im_core/src/entities/rooms/room_dto.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 import '../../enums/room_type_enum.dart';
+import 'room_dto.dart';
 
 part 'room_detail_dto.g.dart';
 
@@ -22,4 +22,12 @@ class RoomDetailDto extends RoomDto {
     super.creationTime,
     super.memberCount,
   });
+
+  ///FromJson
+  factory RoomDetailDto.fromJson(Map<String, dynamic> json) =>
+      _$RoomDetailDtoFromJson(json);
+
+  ///ToJson
+  @override
+  Map<String, dynamic> toJson() => _$RoomDetailDtoToJson(this);
 }
