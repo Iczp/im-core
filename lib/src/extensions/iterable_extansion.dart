de6411edb4f95d349fea4fc656178ca72bd0cefa
zhongpei <1000@intry.cn>
2023-01-14 10:42:00 +0800

    max,min

--- lib/src/extensions/iterable_extansion.dart
+++ lib/src/extensions/iterable_extansion.dart
@@ -16,4 +16,30 @@ extension IterableExtansion<T> on Iterable<T> {
     return cast<T?>()
         .firstWhere((v) => v != null && test(v), orElse: () => null);
   }
+
+  ///
+  T? max(dynamic Function(T) act) {
+    if (length == 0) {
+      return null;
+    }
+    var maxItem = reduce((curr, next) =>
+        (act(curr) as num).toDouble() > (act(next) as num).toDouble()
+            ? curr
+            : next);
+
+    return maxItem;
+  }
+
+  ///
+  T? min(dynamic Function(T) act) {
+    if (length == 0) {
+      return null;
+    }
+    var maxItem = reduce((curr, next) =>
+        (act(curr) as num).toDouble() < (act(next) as num).toDouble()
+            ? curr
+            : next);
+
+    return maxItem;
+  }
 }
